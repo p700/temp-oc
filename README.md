27316b6dfde281edf4d7881bc27b322eb8ca81c1
朱凯 <136212068@qq.com>
2016-06-29 15:10:00 +0800

    no message

---
+++ README.md
@@ -0,0 +1,10 @@
+<h2>OC-TEMP</h2>
+
+>使用 MVC 目录结构
+>自带 PCH 文件
+>使用 Pod 管理第三方框架
+>>Pod 自带 Masonry(自动布局)
+
+>info.plist
+>>支持 http:// 协议
+
---
+++ temp/MainClasses/Controller/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  temp
+//
+//  Created by 朱凯 on 16/6/13.
+//  Copyright © 2016年 朱凯. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
